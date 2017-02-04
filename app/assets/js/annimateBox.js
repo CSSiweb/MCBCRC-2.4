2cb087319a5b51f6f77b644c4874abf648d532ed
Clara <seehuesdesign@gmail.com>
2017-02-04 13:08:30 -0500

    trying a PUSH, 1:08pm

---
+++ app/assets/js/annimateBox.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+$(document).ready(function() {
+
+$(window).scroll(function() {
+
+  if ($(window).scrollTop() > 500) { $('#anything').slideDown(500); $('#anotherthing').fadeIn(1000);
+  }
+  if ($(window).scrollTop() < 500) { $('#anything').slideUp(500); $('#anotherthing').fadeOut(1000);
+  }
+});
+});
